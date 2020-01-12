b2994cd353e9dd986ee463bb19fac725f9c4a23a
David Montes <david.montes@sjsu.edu>
2020-01-11 17:03:15 -0800

    connect IBOutlets

--- Places/PlacesViewController.swift
+++ Places/PlacesViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class PlacesViewController: UIViewController {
 
+    @IBOutlet var mapView:MKMapView?
+    @IBOutlet var tableView:UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
