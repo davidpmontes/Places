e473bb0d85cda9aeaf60b65d7a46fe33e45a23b4
David Montes <david.montes@sjsu.edu>
2020-01-12 12:08:23 -0800

    printing out GPS locations

--- Places/PlacesViewController.swift
+++ Places/PlacesViewController.swift
@@ -9,16 +9,28 @@
 import UIKit
 import MapKit
 
-class PlacesViewController: UIViewController {
+class PlacesViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet var mapView:MKMapView?
     @IBOutlet var tableView:UITableView?
     
+    var locationManager:CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization()
+        
+        locationManager?.delegate = self
+        locationManager?.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        if let newLocation = locations.last {
+            print(newLocation)
+        }
+    }
 }
 
