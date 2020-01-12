25408613c774ac5e28bf33b4138c694f94912c73
David Montes <david.montes@sjsu.edu>
2020-01-12 12:42:25 -0800

    Fixed issue with constraints, map now appears

--- Places/PlacesViewController.swift
+++ Places/PlacesViewController.swift
@@ -22,15 +22,27 @@ class PlacesViewController: UIViewController, CLLocationManagerDelegate {
         locationManager = CLLocationManager()
         locationManager?.requestWhenInUseAuthorization()
         
+        locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager!.distanceFilter = 50.0
+        
         locationManager?.delegate = self
         locationManager?.startUpdatingLocation()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
-        if let newLocation = locations.last {
-            print(newLocation)
+        guard mapView != nil else {
+            return
         }
+        
+        guard let newLocation = locations.last else {
+            return
+        }
+        
+        let region = MKCoordinateRegion(center: newLocation.coordinate, latitudinalMeters: 200, longitudinalMeters: 200)
+        let adjustedRegion = mapView!.regionThatFits(region)
+        
+        mapView!.setRegion(adjustedRegion, animated: true)
     }
 }
 
