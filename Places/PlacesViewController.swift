b5d19a141f3a148d70744f5730438e75d5f2c188
David Montes <david.montes@sjsu.edu>
2020-01-12 18:30:06 -0800

    added call to foursquare API

--- Places/PlacesViewController.swift
+++ Places/PlacesViewController.swift
@@ -16,6 +16,10 @@ class PlacesViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager:CLLocationManager?
     
+    var places = [[String: Any]]()
+    
+    var isQueryPending = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -43,6 +47,54 @@ class PlacesViewController: UIViewController, CLLocationManagerDelegate {
         let adjustedRegion = mapView!.regionThatFits(region)
         
         mapView!.setRegion(adjustedRegion, animated: true)
+        
+        queryFoursquare(location: newLocation)
+    }
+    
+    func queryFoursquare(location: CLLocation)
+    {
+        if isQueryPending == true {
+            return
+        }
+        
+        isQueryPending = true
+        
+        let clientId        = URLQueryItem(name: "client_id", value: FourSquareAPI().ClientID)
+        let clientSecret    = URLQueryItem(name: "client_secret", value: FourSquareAPI().ClientSecret)
+        let version         = URLQueryItem(name: "v", value: "20190401")
+        let coordinate      = URLQueryItem(name: "ll", value: "\(location.coordinate.latitude),\(location.coordinate.longitude)")
+        let query           = URLQueryItem(name: "query", value: "coffee")
+        let intent          = URLQueryItem(name: "intent", value: "browse")
+        let radius          = URLQueryItem(name: "radius", value: "250")
+        
+        var urlComponents = URLComponents(string: "https://api.foursquare.com/v2/venues/search")!
+        urlComponents.queryItems = [clientId, clientSecret, version, coordinate, query, intent, radius]
+
+        let task = URLSession.shared.dataTask(with: urlComponents.url!, completionHandler: { data, response, error in
+            if error != nil {
+                print("*** ERROR *** \(error!.localizedDescription)")
+                return
+            }
+            
+            if data == nil || response == nil {
+                print("*** SOMETHING WENT WRONG!!! ***")
+                return
+            }
+            
+            do {
+                let jsonData = try JSONSerialization.jsonObject(with: data!, options: [])
+
+                print(jsonData)
+
+            } catch {
+                print("*** JSON ERROR *** \(error.localizedDescription)")
+                return
+            }
+            
+            self.isQueryPending = false
+        })
+
+        task.resume()
     }
 }
 
